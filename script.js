aaa752a6644f43121062242363781bd80021fc5d
YusufHasan94 <yhpolok@gmail.com>
2023-03-01 21:30:49 +0600

    optimize and reduce line of code

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ for (const shapes of shape) {
         }
     });
 }
-
 //convert string to number
 function convertStringToNumber(str){
     let getNumber= parseFloat(str);
@@ -39,20 +38,30 @@ function calculation3(value1, value2){
     let result = Math.PI*value1*value2;
     return result.toFixed(2);
 }
-//array to store res
-//adding result into display
+
+//adding result into calculation area
+let count = 0;
 function addingResult(name, res){
-    console.log(name, res);
+    count++;
     let resContainer = document.getElementById('ResultsContainer');
-    let newContainer = document.createElement('li');
+    let newContainer = document.createElement('div');
     newContainer.innerHTML = `
         <div class="flex justify-between items-center my-1 px-2">
+            <span class="text-xl">${count}.</span>
             <span class="text-xl">${name}</span>
             <span class="text-xl">${res} cm<sup>2</sup></span>
             <button class="p-2 bg-sky-600 text-white text-lg rounded ">Convert to m<sup>2<sup></button>
+            <button id="closeBtn" class="bg-red-600 text-white p-2 rounded" onclick="closeFun(event)"><i class="fa-solid fa-xmark"></i></button>
         </div>
     `;
     resContainer.appendChild(newContainer);
+}
+
+//function to delete selected item
+function closeFun(event){
+    count--;
+    let value = event.target.parentNode.parentNode;
+    value.innerHTML='';
 }
 
 //function for getting value and display
@@ -60,7 +69,7 @@ function getValues(inputField1, inputField2, displayField1, displayField2, displ
     let valueField1 = document.getElementById(`${inputField1}`).value;
     let valueField2 = document.getElementById(`${inputField2}`).value;
     let value1 = convertStringToNumber(valueField1);
-    let value2 = convertStringToNumber(valueField2);
+    let value2 = convertStringToNumber(valueField2);   
     if((valueField1 =='') || (valueField2 =='')){
         alert("Enter valid number");
     }
@@ -75,7 +84,6 @@ function getValues(inputField1, inputField2, displayField1, displayField2, displ
                 display2.innerText = value2; 
                 let newInputArea = document.getElementById(`${inputArea}`);
                 newInputArea.style.display = 'none';
-                console.log(typeof(value1), value1,  typeof(value2), value2);
             }
             else{
                 alert("Number should be positive");
@@ -84,8 +92,8 @@ function getValues(inputField1, inputField2, displayField1, displayField2, displ
         else{
             alert("String not acceptable");
         }
-    }
-    return {valueField1, valueField2, value1, value2};
+    } 
+    return{valueField1,valueField2,value1, value2};
 }
 //function for edit values
 function editValues(inputArea, displayArea){
@@ -95,188 +103,86 @@ function editValues(inputArea, displayArea){
     newDisplayArea.style.display = 'none';
 }
 
-//triangle 
-let triangleCalculateSubmit = document.getElementById('triangleCalculate');
-triangleCalculateSubmit.addEventListener('click',function(){
-    let values = getValues('TriangleField1', 'TriangleField2', 'triangleDisplay1', 'triangleDisplay2', 'triangleValueArea', 'triangleInputArea');
-    let newValue1 = values.value1;
-    let newValue2 = values.value2;
-    if(values.valueField1 == '' || values.valueField2 == ''){
-        alert("Enter valid number");
-    }
-    else{
-        if(typeof(newValue1) =='number' && typeof(newValue2) == 'number'){
-            if(newValue1>=0 && newValue2>=0){
-                let result = calculation1(newValue1, newValue2);
-                addingResult('Triangle', result)
-                console.log('Triangle', typeof(newValue1), newValue1, typeof(newValue1), newValue2, "result: ", result);
+//calculate results
+let calculate = document.querySelectorAll('.calculationBtn');
+calculate.forEach((element) => {
+    element.addEventListener('click',(event)=>{
+        let shape = event.target.parentNode.parentNode.childNodes[3].childNodes[1].innerText;
+        if(shape == 'Triangle'){
+            let values = getValues('TriangleField1', 'TriangleField2', 'triangleDisplay1', 'triangleDisplay2', 'triangleValueArea', 'triangleInputArea');
+            if(values.valueField1 != '' || values.valueField2 != '' ){
+                if(values.value1>=0 && values.value2>=0){
+                    let result = calculation1(values.value1, values.value2);
+                    addingResult(`${shape}`, result);
+                }
             }
-            else{
-                alert("Number should be positive");
+        }else if(shape === 'Rectangle'){
+            let values = getValues('rectangleField1', 'rectangleField2', 'rectangleDisplay1', 'rectangleDisplay2', 'rectangleValueArea', 'rectangleInputArea');
+            if(values.valueField1 != '' || values.valueField2 != '' ){
+                if(values.value1>=0 && values.value2>=0){
+                    let result = calculation2(values.value1, values.value2);
+                    addingResult(`${shape}`, result);
+                }
             }
-        }
-        else{
-            alert("String not acceptable");
-        }
-    }
-});
-
-let editTriangleValue = document.getElementById('editTriangleValue');
-editTriangleValue.addEventListener('click', function(){
-    editValues('triangleInputArea', 'triangleValueArea');
-});
-
-//rectangle
-let rectangleCalculateSubmit = document.getElementById('rectangleCalculate');
-rectangleCalculateSubmit.addEventListener('click', function(){
-    let values = getValues('rectangleField1', 'rectangleField2', 'rectangleDisplay1', 'rectangleDisplay2', 'rectangleValueArea', 'rectangleInputArea');
-    let newValue1 = values.value1;
-    let newValue2 = values.value2;
-    if(values.valueField1 == '' || values.valueField2 == ''){
-        alert("Enter valid number");
-    }
-    else{
-        if(typeof(newValue1) =='number' && typeof(newValue2) == 'number'){
-            if(newValue1>=0 && newValue2>=0){
-                let result = calculation2(newValue1, newValue2);
-                addingResult('Rectangle', result)
-                console.log('Rectangle', typeof(newValue1), newValue1, typeof(newValue1), newValue2, "result: ", result);
+        }else if(shape == 'Parallelogram'){
+            let values = getValues('parallelogramField1', 'parallelogramField2', 'parallelogramDisplay1', 'parallelogramDisplay2', 'parallelogramValueArea', 'parallelogramInputArea');
+            if(values.valueField1 != '' || values.valueField2 != '' ){
+                if(values.value1>=0 && values.value2>=0){
+                    let result = calculation2(values.value1, values.value2);
+                    addingResult(`${shape}`, result);
+                }
             }
-            else{
-                alert("Number should be positive");
+        }else if(shape == 'Rhombus'){
+            let values = getValues('rhombusField1', 'rhombusField2', 'rhombusDisplay1', 'rhombusDisplay2', 'rhombusValueArea', 'rhombusInputArea');
+            if(values.valueField1 != '' || values.valueField2 != '' ){
+                if(values.value1>=0 && values.value2>=0){
+                    let result = calculation1(values.value1, values.value2);
+                    addingResult(`${shape}`, result);
+                }
             }
-        }
-        else{
-            alert("String not acceptable");
-        }
-    }
-});
-
-let editRectangleValue = document.getElementById('editRectangleValue');
-editRectangleValue.addEventListener('click', function(){
-    editValues('rectangleInputArea', 'rectangleValueArea');
-});
-
-//parallelogram
-let parallelogramCalculateSubmit = document.getElementById('parallelogramCalculate');
-parallelogramCalculateSubmit.addEventListener('click',function(){
-    let values = getValues('parallelogramField1', 'parallelogramField2', 'parallelogramDisplay1', 'parallelogramDisplay2', 'parallelogramValueArea', 'parallelogramInputArea');
-    let newValue1 = values.value1;
-    let newValue2 = values.value2;
-    if(values.valueField1 == '' || values.valueField2 == ''){
-        alert("Enter valid number");
-    }
-    else{
-        if(typeof(newValue1) =='number' && typeof(newValue2) == 'number'){
-            if(newValue1>=0 && newValue2>=0){
-                let result = calculation2(newValue1, newValue2);
-                addingResult('Parallelogram', result);
-                console.log('Parallelogram', typeof(newValue1), newValue1, typeof(newValue1), newValue2, "result: ", result);
+        }else if(shape == 'Pentagon'){
+            let values = getValues('pentagonField1', 'pentagonField2', 'pentagonDisplay1', 'pentagonDisplay2', 'pentagonValueArea', 'pentagonInputArea');
+            if(values.valueField1 != '' || values.valueField2 != '' ){
+                if(values.value1>=0 && values.value2>=0){
+                    let result = calculation1(values.value1, values.value2);
+                    addingResult(`${shape}`, result);
+                }
             }
-            else{
-                alert("Number should be positive");
+        }else if(shape == 'Ellipse'){
+            let values = getValues('ellipseField1', 'ellipseField2', 'ellipseDisplay1', 'ellipseDisplay2', 'ellipseValueArea', 'ellipseInputArea');
+            if(values.valueField1 != '' || values.valueField2 != '' ){
+                if(values.value1>=0 && values.value2>=0){
+                    let result = calculation3(values.value1, values.value2);
+                    addingResult(`${shape}`, result);
+                }
             }
         }
         else{
-            alert("String not acceptable");
+            console.log("No shapes Available.");
         }
-    }
-    
-});
-let editParallelogramValue = document.getElementById('editParallelogramValue');
-editParallelogramValue.addEventListener('click', function(){
-    editValues('parallelogramInputArea', 'parallelogramValueArea');
+    });
 });
 
-
-//Rhombus
-let rhombusCalculateSubmit = document.getElementById('rhombusCalculate');
-rhombusCalculateSubmit.addEventListener('click',function(){
-    let values = getValues('rhombusField1', 'rhombusField2', 'rhombusDisplay1', 'rhombusDisplay2', 'rhombusValueArea', 'rhombusInputArea');
-    let newValue1 = values.value1;
-    let newValue2 = values.value2;
-    if(values.valueField1 == '' || values.valueField2 == ''){
-        alert("Enter valid number");
-    }
-    else{
-        if(typeof(newValue1) =='number' && typeof(newValue2) == 'number'){
-            if(newValue1>=0 && newValue2>=0){
-                let result = calculation1(newValue1, newValue2);
-                addingResult('Rhombus', result);
-                console.log('Rhombus', typeof(newValue1), newValue1, typeof(newValue1), newValue2, "result: ", result);
-            }
-            else{
-                alert("Number should be positive");
-            }
+//edit values
+let edit = document.querySelectorAll('.editValues');
+edit.forEach((element) => {
+    element.addEventListener('click',(event)=>{
+        let shape = event.target.parentNode.parentNode.parentNode.parentNode.childNodes[1].innerText;
+        if(shape === 'Triangle'){
+            editValues('triangleInputArea', 'triangleValueArea');
+        }else if(shape === 'Rectangle'){
+            editValues('rectangleInputArea', 'rectangleValueArea');
+        }else if(shape == 'Parallelogram'){
+            editValues('parallelogramInputArea', 'parallelogramValueArea');
+        }else if(shape == 'Rhombus'){
+            editValues('rhombusInputArea', 'rhombusValueArea');
+        }else if(shape == 'Pentagon'){
+            editValues('pentagonInputArea', 'pentagonValueArea');
+        }else if(shape == 'Ellipse'){
+            editValues('ellipseInputArea', 'ellipseValueArea');
         }
         else{
-            alert("String not acceptable");
+            console.log("No shapes Available.");
         }
-    }
-});
-
-let editRhombusValue = document.getElementById('editRhombusValue');
-editRhombusValue.addEventListener('click', function(){
-    editValues('rhombusInputArea', 'rhombusValueArea');
-});
-
-//Pentagon
-let pentagonCalculateSubmit = document.getElementById('pentagonCalculate');
-pentagonCalculateSubmit.addEventListener('click',function(){
-    let values = getValues('pentagonField1', 'pentagonField2', 'pentagonDisplay1', 'pentagonDisplay2', 'pentagonValueArea', 'pentagonInputArea');
-    let newValue1 = values.value1;
-    let newValue2 = values.value2;
-    if(values.valueField1 == '' || values.valueField2 == ''){
-        alert("Enter valid number");
-    }
-    else{
-        if(typeof(newValue1) =='number' && typeof(newValue2) == 'number'){
-            if(newValue1>=0 && newValue2>=0){
-                let result = calculation1(newValue1, newValue2);
-                addingResult('Pentagon', result);
-                console.log('Pentagon', typeof(newValue1), newValue1, typeof(newValue1), newValue2, "result: ", result);
-            }
-            else{
-                alert("Number should be positive");
-            }
-        }
-        else{
-            alert("String not acceptable");
-        }
-    }
-});
-let editPentagonValue = document.getElementById('editPentagonValue');
-editPentagonValue.addEventListener('click', function(){
-    editValues('pentagonInputArea', 'pentagonValueArea');
-});
-
-
-//ellipse
-let ellipseCalculate = document.getElementById('ellipseCalculate');
-ellipseCalculate.addEventListener('click',function(){
-    let values = getValues('ellipseField1', 'ellipseField2', 'ellipseDisplay1', 'ellipseDisplay2', 'ellipseValueArea', 'ellipseInputArea');
-    let newValue1 = values.value1;
-    let newValue2 = values.value2;
-    if(values.valueField1 == '' || values.valueField2 == ''){
-        alert("Enter valid number");
-    }
-    else{
-        if(typeof(newValue1) =='number' && typeof(newValue2) == 'number'){
-            if(newValue1>=0 && newValue2>=0){
-                let result = calculation3(newValue1, newValue2);
-                addingResult('Ellipse', result);
-                console.log('Ellipse', typeof(newValue1), newValue1, typeof(newValue1), newValue2, "result: ", result);
-            }
-            else{
-                alert("Number should be positive");
-            }
-        }
-        else{
-            alert("String not acceptable");
-        }
-    }
-});
-let editEllipseValue = document.getElementById('editEllipseValue');
-editEllipseValue.addEventListener('click', function(){
-    editValues('ellipseInputArea', 'ellipseValueArea');
+    });
 });
